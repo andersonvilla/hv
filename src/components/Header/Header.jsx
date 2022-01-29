ceca76de080bf94d2cfe0d7b48b754c6ac0db4cd
andersonvilla <estiven.villa@udea.edu.co>
2022-01-28 23:02:19 -0500

    profile grid

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
       <header className ='bg-[#57C8B4] flex flex-row justify-around'>
         <div className='flex flex-col justify-center '>
-          <h1 className='font-bold text-center text-base'>ANDERSON ESTIVEN VILLA SIERRA</h1>
+          <h1 className='font-bold text-center text-base md:text-2xl'>ANDERSON ESTIVEN VILLA SIERRA</h1>
           <h3 className='font-semibold text-center text-sm'>Estudiante de Ingeniería de Sistemas</h3>
         </div>  
         <img src={foto} className='w-4/12 m-2 '/>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,38 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
-    return (
-        <div className='justify-center'>
-          <div className='m-4'>
-            <h1 className='font-bold text-[#57C8B4] text-center'>Descripción del Perfil</h1>
-            <h3 className='font-semibold text-[#57C8B4] text-center'>Resumen Profesional</h3>
-            <p className='text-justify'>Soy estudiante de ingeniería de sistemas, con conocimientos técnicos y experiencia en el área deback end. 
-              Tengo experiencia en microservicios y base de datos relacionales, y no relacionales. 
-              Apasionado por el auto-estudio y el aprendizaje continuo, me motiva mucho trabajar en equipo y buscar nuevas soluciones.
-            </p>
-          </div> 
-          <hr className='border-1 border-[#57C8B4] w-full'/>
-          <h3 className='font-semibold text-[#57C8B4] text-center'>Skills</h3>
+  return (
+    <div className="justify-center">
+      <div className="m-4">
+        <h1 className="font-bold text-[#57C8B4] text-center">
+          Descripción del Perfil
+        </h1>
+        <div className="md:grid md:grid-cols-2 md:grid-rows-1">
           <div>
-            <ul className='list-disc list-inside m-3'>
+            <h3 className="font-semibold text-[#57C8B4] text-center">
+              Resumen Profesional
+            </h3>
+            <p className="text-justify">
+              Soy estudiante de ingeniería de sistemas, con conocimientos técnicos
+              y experiencia en el área deback end. Tengo experiencia en
+              microservicios y base de datos relacionales, y no relacionales.
+              Apasionado por el auto-estudio y el aprendizaje continuo, me motiva
+              mucho trabajar en equipo y buscar nuevas soluciones.
+            </p>
+          </div>
+          <hr className="border-1 border-[#57C8B4] w-full md:hidden" />
+          <div>
+            <h3 className="font-semibold text-[#57C8B4] text-center">Skills</h3>
+            <div>
+              <ul className="list-disc list-inside m-3 md:ml-10">
                 <li>Java</li>
                 <li>SprintBoot</li>
                 <li>Investigación</li>
                 <li>SQL/NoSQL</li>
                 <li>Git</li>
                 <li>Metodologías Ágiles</li>
-            </ul>
-          </div> 
-          <hr className='border-1 border-[#57C8B4] w-full'/>
-          <h3 className='font-semibold text-[#57C8B4] text-center'>Idiomas</h3>
-          <div>
-            <ul className='list-disc list-inside m-3'>
+              </ul>
+            </div>
+            <hr className="border-1 border-[#57C8B4] w-full md:hidden" />
+            <h3 className="font-semibold text-[#57C8B4] text-center">Idiomas</h3>
+            <div>
+              <ul className="list-disc list-inside m-3 md:ml-10">
                 <li>Español nativo</li>
                 <li>Inglés avanzado</li>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
